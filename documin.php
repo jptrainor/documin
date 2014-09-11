34eb4bc1e1a245a8e8b73e2bd84b0887ef84082d
Jim Trainor <jim.trainor@jptrainor.com>
2014-09-11 06:07:39 -0400

    bump version number - forgot to bump for 0.9.2

--- documin.php
+++ documin.php
@@ -1970,7 +1970,7 @@ the Creative Commons Attribution 3.0 License. The <a
 href="http://commons.wikimedia.org/wiki/File:Replacement_filing_cabinet.svg">file
 cabinet image</a> is in the public domain.</p>
 
-<p>Version 0.9.1 (3 June 2014)</p>
+<p>Version 0.9.3 (11 Sept 2014)</p>
 <hr>
 Admin commands:
 <ul>
